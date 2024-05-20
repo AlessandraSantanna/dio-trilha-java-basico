4098e555f0ec32f60143a0ff133d4416f9f4a4e8
seu-username <seu-email>
2024-05-20 11:45:02 -0300

    codigo ok

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        Scanner sc = new Scanner(System.in);
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+
+
+
+      // exibir as mensagens para os usuários
+
+      System.out.println("------CONTA BANCARIA--------");
+      System.out.println();
+      System.out.println("Por Favor,digite o número de sua conta:");
+      numero= sc.nextInt();
+
+      System.out.println("Agora digite o número de sua agencia:");
+      agencia= sc.next();
+
+      System.out.println("Informe seu Nome:");
+      nomeCliente= sc.next();
+      sc.nextLine();
+      
+
+      System.out.println("Agora informe o sue Saldo Atual");
+      saldo= sc.nextDouble();
+
+      //obter pelo Scanner os valores digitados no terminal 
+       //exibir a mensagem conta criada
+       System.out.println();
+       System.out.printf("Olá %s , obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+       System.out.println();
+
+       
+       sc.close();
     }
 }
+
+
+    
+
+
+
+
+
+
+
+
+    
+     
+
